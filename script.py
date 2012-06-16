b34cf1f35551d17cf95769a381d500160889e54c
Giorgio <imatesiu@gmail.com>
2012-06-16 19:30:24 +0200

    fix script ordered csv

--- script.py
+++ script.py
@@ -3,7 +3,8 @@ import random
 import time
 import os
 import sys
-
+import csv
+from datetime import datetime
 import fileinput
 
 
@@ -11,8 +12,26 @@ if len(sys.argv) < 2:
     print "script.py <namefile>"
     exit(1)
     
+
+
+spamReader = list(csv.reader(open(sys.argv[1]), delimiter=';'))
+del spamReader[0]
+
+def date_key(row):
+        return datetime.strptime(row[2].strip(), "%d-%m-%Y %H:%M")
+        
+
+spamReader.sort(key=date_key)
+
+#for row in spamReader:
+#	print row
+#      writer = csv.writer(open("test1.csv", "wb"))
+#      writer.writerows(row)
+#print spamReader
+   
+#exit(1)
     
-#for line in fileinput.input(sys.argv[1]):
+#for line in fileinput.input(sys.argv[1]): dd-MM-yyyy HH:mm
 #	words= line.split(";")
 #	for word in words:
 		#print word
@@ -29,22 +48,26 @@ def generate_event(res, cicle, name,time):
 """ % (res, cicle, name,time)
 		
 map={}
-i=0
-for line in fileinput.input(sys.argv[1]):
-	if(i!=0):
-		words= line.split(";")
+
+for line in spamReader:
+		words= line
 		nametrace=words[0]
 		name=words[4]
+		name = name.replace(' ', '-')
+		name = name.replace('--', '-')
+		name = name.replace('--', '-')
+		#name = name.replace(' ', '-')
 		time=words[2]
 		resource=words[5]+'/'+words[7]
-		if(map.has_key(nametrace)):
-			event=map[nametrace]
-			event=event+generate_event(resource, "complete", name,time)
-			map[nametrace]=event
-		else:
-			event=generate_event(resource, "complete", name,time)
-			map[nametrace]=event
-	i=i+1
+		if(name!=''):
+			if(map.has_key(nametrace)):
+				event=map[nametrace]
+				event=event+generate_event(resource, "complete", name,time)
+				map[nametrace]=event
+			else:
+				event=generate_event(resource, "complete", name,time)
+				map[nametrace]=event
+	
 		
 
 		
